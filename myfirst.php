3f2c314646bfa2ea73abfac0c6d970e6ca48be3a
kartikcpp <kartiksaini8881@gmail.com>
2021-02-11 12:26:10 +0530

    first commit

---
+++ myfirst.php
@@ -0,0 +1,25 @@
+<html>
+<body>
+<?php 
+// in php a variable starts with $ sign 
+
+# this is also a single line comment
+echo "<h1>hello world</h1>";
+$myname="kartik";
+Echo $myname."  yo";
+
+echo "Hello by PHP echo  
+this is multi line  
+text printed by   
+PHP echo statement  
+";  
+
+$msg="Hello JavaTpoint PHP";  
+echo "Message is: $msg";  
+
+$fname = "Gunjan";  
+$lname = "Garg";  
+print "My name is: ".$fname;
+?>
+</vody>
+</html>
